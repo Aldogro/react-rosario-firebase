625bf0bc4afe1f4b5aa96d60cf611f1a2fd49d06
Aldo Rojas <aldogrojas5@gmail.com>
2023-10-25 17:05:25 -0300

    cleanup branch 02

--- src/Main.js
+++ src/Main.js
@@ -7,10 +7,6 @@ const Main = () => {
             <div className="links-wrapper">
                 <div className="links-group">
                     <Link to="/first-step">First Step</Link>
-                    <Link to="/second-step">Second Step</Link>
-                    <Link to="/third-step">Third Step</Link>
-                    <Link to="/fourth-step">Fourth Step</Link>
-                    <Link to="/fifth-step">Fifth Step</Link>
                 </div>
                 <div className="links-group">
                     <Link to="/login">Login</Link>
@@ -18,10 +14,6 @@ const Main = () => {
             </div>
             <Routes>
                 <Route path="/first-step" element={<FirstStep />} />
-                <Route path="/second-step" element={<FirstStep />} />
-                <Route path="/third-step" element={<FirstStep />} />
-                <Route path="/fourth-step" element={<FirstStep />} />
-                <Route path="/fifth-step" element={<FirstStep />} />
                 <Route path="/login" element={<FirstStep />} />
             </Routes>
         </div>
