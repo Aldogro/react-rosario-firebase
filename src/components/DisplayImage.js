787d2b29f081613027fb3a6c17be84a49ba131c9
Aldo Rojas <aldogrojas5@gmail.com>
2023-10-25 17:33:08 -0300

    updated branch 05

--- src/components/DisplayImage.js
+++ src/components/DisplayImage.js
@@ -1,17 +1,32 @@
+import { useState } from 'react';
 import { deleteObject, storage, storageRef } from '../firebase/firebase';
 import { useDownloadURL } from 'react-firebase-hooks/storage';
 import DisplayError from './DisplayError';
 
 const DisplayImage = ({ imageRef, refetch }) => {
     const [value, loading, error] = useDownloadURL(storageRef(storage, (imageRef && imageRef.fullPath) || '/image'));
+    const [confirmDelete, setConfirmDelete] = useState(false);
 
     const onDelete = async () => {
         try {
             await deleteObject(imageRef);
-            refetch()
+            refetch();
         } catch (error) {
             console.error(error);
         }
+    }
+
+    const ShouldDelete = () => {
+        return (
+            <div className="buttons-container">
+                <button className="button button-cancel" onClick={() => setConfirmDelete(false)}>
+                    Cancel
+                </button>
+                <button className="button button-danger" onClick={onDelete}>
+                    Yes, delete!
+                </button>
+            </div>
+        );
     }
 
     return (
@@ -23,7 +38,14 @@ const DisplayImage = ({ imageRef, refetch }) => {
                         {value && (
                             <img src={value} alt="loaded" />
                         )}
-                        <button className="button button-danger" onClick={onDelete}>Delete</button>
+                        {confirmDelete
+                            ? (<ShouldDelete />)
+                            : (
+                                <button className="button button-danger" onClick={() => setConfirmDelete(true)}>
+                                    Delete
+                                </button>
+                            )
+                        }
                     </div>
                 )
             }
