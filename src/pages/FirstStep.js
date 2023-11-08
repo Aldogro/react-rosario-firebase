c638f320090a1c6ad8e58ac6e42ef5a774b03e7a
Aldo Rojas <aldogrojas5@gmail.com>
2023-11-08 09:50:18 -0300

    added missing qrs

--- src/pages/FirstStep.js
+++ src/pages/FirstStep.js
@@ -1,24 +1,64 @@
+import { useState } from 'react';
 import qrReactFirebaseHooksDocs from '../assets/qr-react-firebase-hooks-docs.svg';
+import qrRepo from '../assets/qr-repo.svg';
+import qrProject from '../assets/qr-project.svg';
 
-const FirstStep = () => {
+const Layout = () => {
+    const [openQR, setOpenQR] = useState('');
     return (
-        <div className="step">
-            <h1>
-                Setting up the project
-            </h1>
-            <h2><b>BRANCH:</b> 01-project-set-up</h2>
-            <div className="divider"></div>
-            <div className="content">
-                <h2>QR to "react-firebase-hooks" Docs</h2>
-                <img
-                    src={qrReactFirebaseHooksDocs}
-                    className="image"
-                    alt="react-firebase-hooks-docs-qr"
-                    height={500}
-                />
-            </div>
+        <div className="app">
+        <h1>
+            Setting up the project
+        </h1>
+        <div style={{ display: 'flex', gap: '8px' }}>
+            <button onClick={() => setOpenQR('qr-react-firebase-hooks')}>
+            QR react-firebase-hooks
+            </button>
+            <button onClick={() => setOpenQR('qr-project')}>
+            Deployed Project
+            </button>
+            <button onClick={() => setOpenQR('qr-repo')}>
+            Repo
+            </button>
+        </div>
+        <dialog open={openQR === 'qr-react-firebase-hooks'}>
+            <h2>react-firebase-hooks Docs</h2>
+            <img
+            src={qrReactFirebaseHooksDocs}
+            className="image"
+            alt="react-firebase-hooks-docs-qr"
+            height={500}
+            />
+            <form method="dialog">
+            <button onClick={() => setOpenQR('')}>Close</button>
+            </form>
+        </dialog>
+        <dialog open={openQR === 'qr-project'}>
+            <h2>Link to the deployed project</h2>
+            <img
+            src={qrProject}
+            className="image"
+            alt="react-firebase-hooks-docs-qr"
+            height={500}
+            />
+            <form method="dialog">
+            <button onClick={() => setOpenQR('')}>Close</button>
+            </form>
+        </dialog>
+        <dialog open={openQR === 'qr-repo'}>
+            <h2>Link to Github Repo</h2>
+            <img
+            src={qrRepo}
+            className="image"
+            alt="repo-qr"
+            height={500}
+            />
+            <form method="dialog">
+            <button onClick={() => setOpenQR('')}>Close</button>
+            </form>
+        </dialog>
         </div>
     );
 };
 
-export default FirstStep;
+export default Layout;
